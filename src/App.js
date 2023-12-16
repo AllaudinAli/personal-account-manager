b0282b530e9653ccf0ea4cb3838cde88679b6c5d
Allaudin Ali Ahmad <ahmadcrisp@gmail.com>
2023-12-16 18:02:09 +0500

    made a personal account manager for my assignment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Balance from "./components/Balance";
+import IncomeExpenses from "./components/IncomeExpenses";
+import TransactionHistory from "./components/TransactionHistory";
+import AddTransaction from "./components/AddTransaction";
+import "./styles.css";
 
-function App() {
+const App = () => {
+  const [transactions, setTransactions] = useState([]);
+
+  const addTransaction = (transaction) => {
+    setTransactions([...transactions, transaction]);
+  };
+
+  const deleteTransaction = (id) => {
+    setTransactions(
+      transactions.filter((transaction) => transaction.id !== id)
+    );
+  };
+
+  const getIncome = () =>
+    transactions
+      .filter((item) => item.amount > 0)
+      .reduce((acc, item) => (acc += item.amount), 0);
+
+  const getExpense = () =>
+    transactions
+      .filter((item) => item.amount < 0)
+      .reduce((acc, item) => (acc += item.amount), 0);
+
+  const getBalance = () =>
+    transactions.reduce((acc, item) => (acc += item.amount), 0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <div className="app">
+        <Balance balance={getBalance()} />
+        <IncomeExpenses income={getIncome()} expense={getExpense()} />
+        <TransactionHistory
+          transactions={transactions}
+          deleteTransaction={deleteTransaction}
+        />
+        <AddTransaction addTransaction={addTransaction} />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
